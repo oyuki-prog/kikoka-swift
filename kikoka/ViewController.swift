dd8e537d09a1a0aa542de561b34f72bddf10c4b7
oyuki-prog <oyu8no3jo@gmail.com>
2021-11-27 09:33:32 +0900

    Initial Commit

---
+++ kikoka/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kikoka
+//
+//  Created by o.yuki on 2021/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
